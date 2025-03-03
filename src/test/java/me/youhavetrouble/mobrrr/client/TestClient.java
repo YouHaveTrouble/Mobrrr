9127e0642ff7dc1c5fb5fde8f2ed6dbfcc1c52af
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2025-03-03 23:41:41 +0100

    handle kick packet

--- src/test/java/me/youhavetrouble/mobrrr/client/TestClient.java
+++ src/test/java/me/youhavetrouble/mobrrr/client/TestClient.java
@@ -3,11 +3,15 @@ package me.youhavetrouble.mobrrr.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+/**
+ * Very, very crude and simple client for testing basic server functionality.
+ */
 public class TestClient {
 
     private static final Logger logger = LoggerFactory.getLogger(TestClient.class);
@@ -21,8 +25,28 @@ public class TestClient {
 
             logger.info("Connected.");
 
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             sendLoginPacket(out);
+
+            socket.setKeepAlive(true);
+
+            boolean reading = true;
+
+            while (reading) {
+                byte id = dataInputStream.readByte();
+                logger.info("Received packet with id {}", id);
+                switch (id) {
+                    case 0:
+                        String reason = dataInputStream.readUTF();
+                        logger.info("Kicked: {}", reason);
+                        reading = false;
+                        break;
+                    default:
+                        logger.warn("Unknown packet id: {}", id);
+                        break;
+                }
+            }
 
             logger.info("Disconnected.");
 
