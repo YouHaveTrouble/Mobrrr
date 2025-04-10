00c7e771606da312989b47f8fef1354d987bc3b6
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2025-04-10 19:37:30 +0200

    convert from record to class to allow extension

--- src/main/java/me/youhavetrouble/mobrrr/server/game/map/terrain/TerrainComponent.java
+++ src/main/java/me/youhavetrouble/mobrrr/server/game/map/terrain/TerrainComponent.java
@@ -4,11 +4,33 @@ import org.jetbrains.annotations.NotNull;
 
 import java.awt.geom.Area;
 
-public record TerrainComponent(@NotNull Area area, double height) {
+public class TerrainComponent {
 
-    public TerrainComponent(@NotNull Area area, double height) {
+    private final Area area;
+    private final double height;
+
+    public TerrainComponent(
+            @NotNull Area area,
+            double height
+    ) {
         this.area = area;
         this.height = height;
+    }
+
+    /**
+     * Returns a copy of the collision area
+     * @return a copy of the collision area
+     */
+    public @NotNull Area area() {
+        return new Area(area);
+    }
+
+    /**
+     * Returns the height of the terrain component
+     * @return the height of the terrain component
+     */
+    public double height() {
+        return height;
     }
 
 }
